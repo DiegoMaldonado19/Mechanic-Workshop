8c10acf2c750a2b8f38c798a6d10f6d0063a75c8
Diego Maldonado <dj_maldonado19@hotmail.es>
2025-08-10 21:23:24 -0600

    Feat: adding repository

---
+++ src/main/java/com/project/ayd/mechanic_workshop/MechanicWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.project.ayd.mechanic_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ayd/mechanic_workshop/MechanicWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ayd.mechanic_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
